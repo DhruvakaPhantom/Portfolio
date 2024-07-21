b669e50a90710c260ff39fa2fede06e3aa7f295b
DhruvakaPhantom <vayakuldhruv78@gmail.com>
2024-07-21 15:15:05 +0530

    made changes in footer

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -9,17 +9,20 @@ const Footer = () => {
                     initial={{ opacity: 0 }}
                     whileInView={{ opacity: 1 }}
                     transition={{ duration: 1.5, ease: "easeInOut" }}
+                    viewport={{ once: true, amount: 0.5 }}
                     className='bg-gradient-to-l from-[#13ADC7] via-[#6978D1] to-[#945DD6] bg-clip-text w-fit text-transparent font-[poppins] text-[19px] font-bold min-[768px]:text-[22px] min-[1440px]:text-[26px]'>Feel free to contact me</motion.h1>
                 <div className='flex  p-2 gap-3 justify-center items-center min-[768px]:gap-6 min-[1440px]:gap-10' >
                     <motion.a
                         initial={{ opacity: 0 }}
                         whileInView={{ opacity: 1 }}
                         transition={{ duration: 1.5, ease: "easeInOut" }}
+                        viewport={{ once: true }}
                         href="https://www.linkedin.com/in/dhruv-vayakul/" target='_blank'><i className="devicon-linkedin-plain text-white text-[25px] min-[768px]:text-[27px] cursor-pointer hover:text-[#929EB0] min-[1440px]:text-[29px] hover:duration-500 duration-500" ></i></motion.a>
                     <motion.a
                         initial={{ opacity: 0 }}
                         whileInView={{ opacity: 1 }}
                         transition={{ duration: 1.5, ease: "easeInOut" }}
+                        viewport={{ once: true }}
                         href="https://github.com/DhruvakaPhantom" target='_blank'><i className="devicon-github-original text-white text-[25px] min-[768px]:text-[27px] cursor-pointer hover:text-[#929EB0] min-[1440px]:text-[29px] hover:duration-500 duration-500"></i></motion.a>
                 </div>
             </div>
@@ -28,6 +31,7 @@ const Footer = () => {
                     initial={{ opacity: 0 }}
                     whileInView={{ opacity: 1 }}
                     transition={{ duration: 1.5, ease: "easeInOut" }}
+                    viewport={{ once: true }}
                     href="https://mail.google.com/mail/?view=cm&fs=1&tf=1&to=vayakuldhruv78@gmail.com" target='_blank' className='font-[poppins] text-white text-[18px]  hover:text-[#929EB0] min-[1440px]:text-[20px] duration-500'>vayakuldhruv78@gmail.com</motion.a>
             </div>
         </div>
